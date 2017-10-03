d678c227caced600c26a4dbaa516485dc64907b7
Kevin <stradtkt22@gmail.com>
2017-10-03 09:18:23 -0400

    Second Commit-made changes to scraper.js

--- scraper.js
+++ scraper.js
@@ -1,10 +1,15 @@
+'use strict';
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const cheerio = require('cheerio');
 const request = require('request');
 const json2csv = require('json2csv');
 const moment = require('moment');
-
+/**
+ * function for handling errors
+ * @constructor
+ * @param {object} error - An error object getting thrown
+*/
 
 function checkDirectory(directory) {
     try {
@@ -13,15 +18,18 @@ function checkDirectory(directory) {
         fs.mkdirSync(directory);
     }
 }
+//makes the directory data
 checkDirectory("./data");
 
-//error messages brought up with certain conditions
-var errorMessage = (error) => console.log("There was an error in the code: ", error);
-//intended url
+
+
+    //makes the directory data
+var errorMessage = function(error) {
+    console.log('There has been an error in the code, this is the error: ', error);
+};
 var contentURL = "http://shirts4mike.com/";
 //Labels for the CSV
 var labels = ['Price', 'Title', 'ImageUrl', 'url', 'Time'];
-
 
 var fieldsArray = new Array();
 request(contentURL, (error, res, body) => {
@@ -31,18 +39,20 @@ request(contentURL, (error, res, body) => {
         var shirtPath = $('.shirts > a').attr('href');
         //combining the shirtPath and contentURL then used in the new request
         var newContentURL = contentURL + shirtPath;
-        request(newContentURL, (error, res, body) => {
+        request(newContentURL, function(error, res, body) {
             if(!error) {
                 var $ = cheerio.load(body);
+            //the last path to the portion of the site
+            //now we will be able to target those elements and send them to the csv file
             var finishedURL = $('.products > li > a').length;
+            //this loops through all the specified elements with index as a parameter
             $(".products > li > a").each(function (index) {
             var secondUrl = ("http://shirts4mike.com/"+ $(this).attr('href'));
 
-            request(secondUrl, (error, req, body) => {
+            request(secondUrl, function(error, req, body) {
                 if(!error) {
-                //Ability to use jQuery
+                //loads the body of the request
                 var $ = cheerio.load(body);
-
                 //title location
                 var title = $('body').find(".shirt-details > h1").text().slice(4);
                 //price location
@@ -69,8 +79,11 @@ request(contentURL, (error, res, body) => {
                 if (fieldsArray.length == finishedURL) {
                     //json2csv transfers the json into a csv file
                         json2csv({ data: fieldsArray, fields: labels }, function(err, csv) {
+                            //if error
                             if(err) return console.log(err);
+                            //writing the csv file
                             fs.writeFile('./data/' + time + '.csv', csv, function(err) {
+                                //if there is an error throw an error
                                 if (err) throw err;
                                 console.log('Your csv file has been saved');
                             });
@@ -79,15 +92,18 @@ request(contentURL, (error, res, body) => {
                     }
                     return fieldsArray;
                 } else {
+                    //error message
                     errorMessage(error);
                 }
             });
         });
         } else {
+            //error message
             errorMessage(error);
         }
         });
     } else {
+        //error message
         errorMessage(error);
     }
 });
